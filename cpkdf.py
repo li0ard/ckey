8656602eed0cfe47f11ef69978bb9c4abad36b28
Lizard <li0ard@proton.me>
2024-07-02 20:55:31 +0600

    refactor: fix license issue & add multihash support to cpkdf

--- cpkdf.py
+++ cpkdf.py
@@ -1,7 +1,5 @@
-from pygost.gost34112012256 import GOST34112012256
-
-def derive(salt: bytes, passphrase: bytes):
-    hasher = GOST34112012256()
+def derive(digestmod, passphrase: bytes, salt: bytes):
+    hasher = digestmod()
     bs = hasher.digest_size * 2
     if len(passphrase) * 4 > 1024:
         raise ValueError("passphrase cannot be longer than 256 symbols")
@@ -16,7 +14,7 @@ def derive(salt: bytes, passphrase: bytes):
     if len(passphrase) != 0:
         hasher.update(pin)
     hash = hasher.digest()
-    hasher = GOST34112012256()
+    hasher = digestmod()
 
     # Создание основого и вторичных массивов
     c = bytearray("DENEFH028.760246785.IUEFHWUIO.EF", "utf-8")
@@ -44,7 +42,7 @@ def derive(salt: bytes, passphrase: bytes):
         c = hasher.digest()
         if len(c) < bs:
             c = c.ljust(bs, b'\x00')
-        hasher = GOST34112012256()
+        hasher = digestmod()
 
     # Третий этап - получаем хэш соли и вторичных массивов
     for i in range(len(c)):
@@ -56,10 +54,11 @@ def derive(salt: bytes, passphrase: bytes):
     hasher.update(m1[0:32])
     if len(passphrase) != 0:
         hasher.update(pin)
-
     c = hasher.digest()
+
+    # Четвёртый этап - хэшируем результат
     if len(c) < bs:
         c = c.ljust(bs, b'\x00')
-    hasher = GOST34112012256()
+    hasher = digestmod()
     hasher.update(c[0:32])
     return hasher.digest()
--- export.py
+++ export.py
@@ -2,22 +2,23 @@ from cpkdf import derive
 from pygost.gost28147 import ecb_decrypt
 from Crypto.Util.number import bytes_to_long, long_to_bytes, inverse
 from pygost.gost3410 import prv_unmarshal, public_key, pub_marshal
+from pygost.gost34112012256 import GOST34112012256
 
 def reverse(d):
     d = d[::-1]
     return d
 
-def getPrimaryKey(salt, mask, prim, passphrase, curve):
+def getPrimaryKey(salt: bytes, mask: bytes, prim: bytes, passphrase: bytes, curve):
     primaryKey = ecb_decrypt(
-        key=derive(salt, passphrase),
+        key=derive(GOST34112012256, passphrase, salt),
         data=prim,
         sbox="id-tc26-gost-28147-param-Z"
     )
-    # Reverse primary key
+    # Реверсим primaryKey
     primaryKey = reverse(primaryKey)
     pk = bytes_to_long(primaryKey)
 
-    # Reverse mask
+    # Реверсим маску
     mask = reverse(mask)
     m = bytes_to_long(mask)
     raw = long_to_bytes((pk * inverse(m, curve.q)).__mod__(curve.q))
