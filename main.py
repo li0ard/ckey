a127bfdf6cfc85244676e9ceb203776b5b22d444
Lizard <li0ard@proton.me>
2024-07-06 14:27:12 +0600

    feat: update oid->curve list

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from getpass import getpass
 from uuid import uuid4
 import sys
 
+# OID -> Curve, upd 06.07.24
 curves = {
     "1.2.643.7.1.2.1.1.1": CURVES["id-tc26-gost-3410-12-256-paramSetA"],
     "1.2.643.7.1.2.1.1.2": CURVES["id-tc26-gost-3410-12-256-paramSetB"],
@@ -18,11 +19,13 @@ curves = {
     "1.2.643.7.1.2.1.2.1": CURVES["id-tc26-gost-3410-12-512-paramSetA"],
     "1.2.643.7.1.2.1.2.2": CURVES["id-tc26-gost-3410-12-512-paramSetB"],
     "1.2.643.7.1.2.1.2.3": CURVES["id-tc26-gost-3410-12-512-paramSetC"],
-    "1.2.643.2.2.35.1": CURVES["id-tc26-gost-3410-12-256-paramSetB"], # CpSet A
-    "1.2.643.2.2.35.2": CURVES["id-tc26-gost-3410-12-256-paramSetC"], # CpSet B
-    "1.2.643.2.2.35.3": CURVES["id-tc26-gost-3410-12-256-paramSetD"], # CpSet C
+    "1.2.643.2.2.35.0": CURVES["id-GostR3410-2001-TestParamSet"],
+    "1.2.643.2.2.35.1": CURVES["id-tc26-gost-3410-12-256-paramSetB"], # CryptoPro Paramset A
+    "1.2.643.2.2.35.2": CURVES["id-tc26-gost-3410-12-256-paramSetC"], # CryptoPro Paramset B
+    "1.2.643.2.2.35.3": CURVES["id-tc26-gost-3410-12-256-paramSetD"], # CryptoPro Paramset C
     "1.2.643.2.2.36.0": CURVES["id-GostR3410-2001-CryptoPro-XchA-ParamSet"],
     "1.2.643.2.2.36.1": CURVES["id-GostR3410-2001-CryptoPro-XchB-ParamSet"],
+    "1.2.643.2.9.1.8.1": CURVES["GostR3410_2001_ParamSet_cc"]
 }
 
 print("ckey by li0ard")
@@ -71,7 +74,7 @@ if fp == bytes(header["keyContainerContent"]["primaryFP"]):
     f.close()
     print("Сохранено в exported_" + uid + ".pem")
 else:
-    print("Ошибка валидации публичного ключа")
+    print("Ошибка валидации публичного ключа\nВозможно вы ввели неправильный пароль.")
     print("Ожидается: " + bytes(header["keyContainerContent"]["primaryFP"]).hex())
     print("Получено: " + fp.hex())
-    print("Если вы считаете что это ошибка - создайте issue")
+    print("Если вы считаете что так быть не должно - создайте issue")
