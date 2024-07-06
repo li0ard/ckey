55be944121b993b199e2149f851fb3159de64f74
Lizard <li0ard@proton.me>
2024-07-06 14:02:55 +0600

    refactor: remove "reverse" and pycryptodome func's

--- export.py
+++ export.py
@@ -1,12 +1,8 @@
 from cpkdf import derive
 from pygost.gost28147 import ecb_decrypt
-from Crypto.Util.number import bytes_to_long, long_to_bytes, inverse
 from pygost.gost3410 import prv_unmarshal, public_key, pub_marshal
 from pygost.gost34112012256 import GOST34112012256
-
-def reverse(d):
-    d = d[::-1]
-    return d
+from pygost.utils import bytes2long, long2bytes, modinvert
 
 def getPrimaryKey(salt: bytes, mask: bytes, prim: bytes, passphrase: bytes, curve):
     primaryKey = ecb_decrypt(
@@ -15,13 +11,13 @@ def getPrimaryKey(salt: bytes, mask: bytes, prim: bytes, passphrase: bytes, curv
         sbox="id-tc26-gost-28147-param-Z"
     )
     # Реверсим primaryKey
-    primaryKey = reverse(primaryKey)
-    pk = bytes_to_long(primaryKey)
+    primaryKey = primaryKey[::-1]
+    pk = bytes2long(primaryKey)
 
     # Реверсим маску
-    mask = reverse(mask)
-    m = bytes_to_long(mask)
-    raw = long_to_bytes((pk * inverse(m, curve.q)).__mod__(curve.q))
-    raw = reverse(raw)
+    mask = mask[::-1]
+    m = bytes2long(mask)
+    raw = long2bytes((pk * modinvert(m, curve.q)).__mod__(curve.q))
+    raw = raw[::-1]
     pub = pub_marshal(public_key(curve, prv_unmarshal(raw)))
     return (raw, pub)
